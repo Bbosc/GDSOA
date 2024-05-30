85d60d05abb92959c613187a51a674bb24987796
Baudouin Bosc <baudouin.bosc@gmail.com>
2024-05-30 10:31:48 +0200

    working at leat without obstacles

---
+++ utils/franka_parameters.py
@@ -0,0 +1,11 @@
+# source : https://frankaemika.github.io/docs/control_parameters.html
+
+joint_limits = [
+    {'lower': -2.8973, 'upper': 2.8973}, # joint 1
+    {'lower': -1.7628, 'upper': 1.7628}, # joint 2
+    {'lower': -2.8973, 'upper': 2.8973}, # joint 3
+    {'lower': -3.0718, 'upper': -0.0698}, # joint 4
+    {'lower': -2.8973, 'upper': 2.8973}, # joint 5
+    {'lower': -0.0175, 'upper': 3.7525}, # joint 6
+    {'lower': -2.8973, 'upper': 2.8973}, # joint 7
+]
